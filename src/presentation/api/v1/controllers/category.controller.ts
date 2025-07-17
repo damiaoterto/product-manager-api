e635eec053babf79050daf6a461697d70302a219
Francisco Damião Terto <contato@damiaoterto.dev>
2025-07-16 22:04:13 -0300

    feat: create new category method

--- src/presentation/api/v1/controllers/category.controller.ts
+++ src/presentation/api/v1/controllers/category.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateCategoryDTO } from '@application/category/dto/create-category.dto';
 
 @Controller({ path: 'categories', version: '1' })
-export class CategoryController {}
+export class CategoryController {
+  @Post()
+  async createNewCategory(@Body() data: CreateCategoryDTO) {
+    // TODO
+  }
+}
