4f1f00418348f1dac613f73976705f47e7c98d3e
Francisco Damião Terto <contato@damiaoterto.dev>
2025-07-17 19:48:16 -0300

    refactor: remove sleep on promise attempt

--- src/utils/promise-attempt.ts
+++ src/utils/promise-attempt.ts
@@ -1,13 +1,10 @@
 import { Logger } from '@nestjs/common';
-import { setTimeout } from 'node:timers/promises';
 
 export async function promiseAttempt(
   name: string,
   promise: () => Promise<void>,
   maxAttempts = 5,
 ) {
-  const sleep = (ms: number) => setTimeout(ms);
-
   const logger = new Logger();
   let attemptCounter = 0;
 
@@ -20,7 +17,6 @@ export async function promiseAttempt(
       logger.error(
         `Error on run ${name} start attempt ${attemptCounter}: ${error.message}`,
       );
-      await sleep(2000);
     }
   }
 }
